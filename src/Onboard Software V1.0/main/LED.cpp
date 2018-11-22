da605a256856b6d4bf9f2109eedc4848f3c7eecd
Carl Baron <cbaron3@uwo.ca>
2018-11-22 21:16:29 +0000

    Added state variable so you can check whether the current LED is on or off

--- src/Onboard Software V1.0/main/LED.cpp
+++ src/Onboard Software V1.0/main/LED.cpp
@@ -4,18 +4,31 @@
 #include "LED.h"
 #include "Config.h"
 
+// Initial constructor. Sets the pin, pinmode, and sets initial state as false
 LED::LED(uint8_t pin){
 	this->pin = pin;
+	state = false;
 	pinMode(pin, OUTPUT);
 
 }
 
 // TODO: Use efficient digital write not the arduino version
+// Turns on the LED and changes state to true
+// Volatile for threading, no compiler optimzations
 void LED::turnOn() volatile{
 	digitalWrite(pin, HIGH);
+	state = true;
 }
 
 // TODO: Use efficient digital write not the arduino version
+// Turns off the LED and changes state to false
+// Volatile for threading, no compiler optimzations
 void LED::turnOff() volatile{
 	digitalWrite(pin, LOW);
+	state = false;
+}
+
+// Get state
+bool getState const{
+	return state;
 }
--- src/Onboard Software V1.0/main/LED.h
+++ src/Onboard Software V1.0/main/LED.h
@@ -6,12 +6,17 @@
 class LED{
 private:
 	uint8_t pin;
+
+	// True means led is ON, false means led is OFF
+	bool state;
+	
 public:
 	LED(uint8_t pin);
 
 	void turnOn() volatile;
-
 	void turnOff() volatile;
+
+	bool getState() const;
 };
 
 #endif
